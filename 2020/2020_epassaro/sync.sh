281b1c6a46bdb6ab58f0ff689c15a5c237db36fa
Ezequiel Pássaro <epassaro15@gmail.com>
2020-05-28 09:08:50 -0300

    Minor changes

--- 2020/2020_epassaro/sync.sh
+++ 2020/2020_epassaro/sync.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
+if [ "$1" == "" ]; then
+    echo; echo "Usage:  sync.sh <MSU_NETID>"
+    exit 1
+fi
+
 rsync -ave ssh $1@rsync.hpcc.msu.edu:/mnt/home/$1/Documents/tardis-setups/2020/2020_epassaro/Output .
+
+exit 0
