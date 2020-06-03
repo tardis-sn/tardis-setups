fdba9a4ae7cd42b0a3697a300397b2aba70e8826
Ezequiel Pássaro <epassaro15@gmail.com>
2020-06-03 17:16:28 -0300

    Update w/changes from chianti-hdf-new branch

--- 2020/2020_epassaro/create_atom_data.py
+++ 2020/2020_epassaro/create_atom_data.py
@@ -2,34 +2,30 @@
 
 import os
 import sys
-import carsus
 from carsus.io.nist import NISTWeightsComp, NISTIonizationEnergies
 from carsus.io.kurucz import GFALLReader
 from carsus.io.zeta import KnoxLongZeta
 from carsus.io.chianti_ import ChiantiReader
 from carsus.io.output import TARDISAtomData
 
-CARSUS_DIR = os.path.dirname(carsus.__file__)
-CARSUS_OUT = os.environ['CARSUS_OUT']
 
-gfall_ions = 'H-Zn'
-gfall_url = 'http://kurucz.harvard.edu/linelists/gfall/gfall.dat'
-os.system('wget -q -O /tmp/gfall.dat {}'.format(gfall_url))
+CARSUS_OUT = os.environ['CARSUS_OUT']
+GFALL_IONS = 'H-Zn'
 
 try:
     chianti_ions = sys.argv[1]
+
 except IndexError:
     chianti_ions = None
 
 parsers = []
-parsers.append(NISTWeightsComp(gfall_ions))
-parsers.append(NISTIonizationEnergies(gfall_ions))
-parsers.append(GFALLReader('/tmp/gfall.dat', gfall_ions))
-parsers.append(KnoxLongZeta(os.path.join(CARSUS_DIR,
-                                         'data/knox_long_recombination_zeta.dat')))
+parsers.append(NISTWeightsComp(GFALL_IONS))
+parsers.append(NISTIonizationEnergies(GFALL_IONS))
+parsers.append(KnoxLongZeta())
+parsers.append(GFALLReader(ions=GFALL_IONS))
 
 if chianti_ions:
-    parsers.append(ChiantiReader(chianti_ions, priority=20))
+    parsers.append(ChiantiReader(ions=chianti_ions, priority=20))
     ions_string = chianti_ions.replace(' ', '_').replace(';', '')
     output_file = 'kurucz_cd23_latest_chianti_{}.h5'.format(ions_string)
 else:
