500232228db2824a47eb198de7889430787ba748
Ezequiel Pássaro <epassaro15@gmail.com>
2020-06-04 00:17:22 -0300

    Trying to fix t_inner and w

--- 2020/2020_epassaro/run_model_toy06.py
+++ 2020/2020_epassaro/run_model_toy06.py
@@ -16,7 +16,7 @@ pattern_remove_bracket = re.compile('\[.+\]')
 t0_pattern = re.compile('tend = (.+)\n')
 
 
-def read_blondin_toymodel(fname):
+def read_blondin_toymodel(fname, t_inner=None, w=None):
     with open(fname, 'r') as fh:
         for line in fh:
             if line.startswith("#idx"):
@@ -49,6 +49,10 @@ def read_blondin_toymodel(fname):
         (new_velocities, [2 * new_velocities[-1] - new_velocities[-2]]))
     blondin_csv['velocity'] = new_velocities
 
+    if t_inner and w:
+        blondin_csv['t_inner'] = t_inner
+        blondin_csv['w'] = w
+
     with open(fname, 'r') as fh:
         t0_string = t0_pattern.findall(fh.read())[0]
 
@@ -63,6 +67,12 @@ def read_blondin_toymodel(fname):
         dict(name='density', unit='g/cm^3', desc='mean density of shell.'))
     blondin_dict_fields.append(
         dict(name='t_rad', unit='K', desc='radiative temperature.'))
+
+    if t_inner and w:
+        blondin_dict_fields.append(
+            dict(name='t_inner', unit='K', desc='inner boundary temperature.'))
+        blondin_dict_fields.append(
+            dict(name='w', desc='dilution factor.'))
 
     for abund in blondin_csv.columns[3:]:
         blondin_dict_fields.append(
@@ -94,25 +104,30 @@ def run_tardis_model(params):
     fname = '{}/toy06_t{}_v{}.h5'.format(
         full_path, params[0].value, params[2].value)
 
-    if 'chianti' not in model_config.atom_data:
-        sim.to_hdf(fname)
-    else:
-        with pd.HDFStore(fname) as hdf:
-            hdf.put('wavelength', pd.Series(
-                sim.runner.spectrum.wavelength.value))
-            hdf.put('luminosity_density_lambda', pd.Series(
-                sim.runner.spectrum_integrated.luminosity_density_lambda.value))
-            hdf.put('w', pd.Series(sim.plasma.w))
-            hdf.put('t_electrons', pd.Series(sim.plasma.t_electrons))
-            hdf.put('ion_number_density', sim.plasma.ion_number_density)
-            hdf.put('electron_densities', sim.plasma.electron_densities)
-            hdf.put('t_rad', pd.Series(sim.plasma.t_rad))
-            hdf.put('r_inner_cgs', pd.Series(sim.runner.r_inner_cgs))
+    with pd.HDFStore(fname) as hdf:
+        hdf.put('wavelength', pd.Series(
+            sim.runner.spectrum.wavelength.value))
+        hdf.put('luminosity_density_lambda', pd.Series(
+            sim.runner.spectrum_integrated.luminosity_density_lambda.value))
+        hdf.put('w', pd.Series(sim.plasma.w))
+        hdf.put('t_electrons', pd.Series(sim.plasma.t_electrons))
+        hdf.put('ion_number_density', sim.plasma.ion_number_density)
+        hdf.put('electron_densities', sim.plasma.electron_densities)
+        hdf.put('t_rad', pd.Series(sim.plasma.t_rad))
+        hdf.put('r_inner_cgs', pd.Series(sim.runner.r_inner_cgs))
+        't_inner', pd.Series(sim.iterations_t_inner)
 
     return 1
 
 
-blondin_dict, blondin_csv = read_blondin_toymodel('snia_toy06.dat')
+try:
+    t_inner = pd.read_hdf(sys.argv[2], 't_inner')
+    w = pd.read_hdf(sys.argv[2], 'w')
+    blondin_dict, blondin_csv = read_blondin_toymodel(
+        'snia_toy06.dat', t_inner, w)
+except IndexError:
+    blondin_dict, blondin_csv = read_blondin_toymodel('snia_toy06.dat')
+
 blondin_dict['v_inner_boundary'] = '9000 km/s'
 blondin_dict['v_outer_boundary'] = '35000 km/s'
 blondin_dict['model_isotope_time_0'] = '0. d'
