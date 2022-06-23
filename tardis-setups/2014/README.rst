9105dea58985c8a45f8c80d9de68c3af5e0ff08f
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2022-06-23 13:28:16 +0530

    Remove additional sphinx gallery output directory and modify the rst

--- tardis-setups/2014/README.rst
+++ tardis-setups/2014/README.rst
@@ -1,4 +1,4 @@
 2014 Examples
 ==================
 
-Below is a gallery of examples
+Below is a gallery of examples.
--- tardis-setups/2014/plot_tardis_example.py
+++ tardis-setups/2014/plot_tardis_example.py
@@ -2,7 +2,7 @@
 Tardis 2014 Example
 ==========================
 
-This is the example
+This is the example output
 """
 
 
@@ -10,6 +10,7 @@ from tardis import run_tardis
 from tardis.io.config_reader import Configuration
 from tardis.io.atom_data.util import download_atom_data
 import matplotlib.pyplot as plt
+import os
 
 # %%
 # Uncomment this line if you need to download the dataset.
@@ -19,14 +20,21 @@ download_atom_data("kurucz_cd23_chianti_H_He")
 
 # %%
 # Runs the example
+def find_file(name, path):
+    for dirpath, _, filename in os.walk(path):
+        if name in filename:
+            return os.path.join(dirpath, name)
+
+
 def config_modifier(conf):
     conf["montecarlo"]["nthreads"] = 1
     conf["montecarlo"]["last_no_of_packets"] = 1.0e5
     conf["montecarlo"]["no_of_virtual_packets"] = 10
 
-    conf["atom_data"] = os.path.join(
-        os.path.expanduser("~"), "Downloads", "tardis-data", conf["atom_data"]
-    )
+    # conf["atom_data"] = os.path.join(
+    #     os.path.expanduser("~"), "Downloads", "tardis-data", conf["atom_data"]
+    # )
+    conf["atom_data"] = find_file(conf["atom_data"], "/")
 
     return conf
 
@@ -35,8 +43,6 @@ conf = Configuration.from_yaml(
     "../../2014/2014_kerzendorf_sim/appendix_A1/tardis_example.yml"
 )
 modified_conf = config_modifier(conf)
-
-print(modified_conf)
 
 # %%
 sim = run_tardis(modified_conf)
